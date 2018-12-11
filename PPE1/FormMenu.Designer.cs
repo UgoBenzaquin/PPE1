3834e052e0db94aa26d4c66d884767521a440fd8
UgoBenzaquin <45796868+UgoBenzaquin@users.noreply.github.com>
2018-12-11 17:49:00 +0100

    ugo
    dland

--- PPE1/FormMenu.Designer.cs
+++ PPE1/FormMenu.Designer.cs
@@ -34,6 +34,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.labelNom = new System.Windows.Forms.Label();
             this.labelID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonPrograme
@@ -94,11 +96,33 @@
             this.labelID.TabIndex = 5;
             this.labelID.Text = "ID";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(390, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(398, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ID";
+            // 
             // FormMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.labelID);
             this.Controls.Add(this.labelNom);
             this.Controls.Add(this.label1);
@@ -120,5 +144,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelNom;
         private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
